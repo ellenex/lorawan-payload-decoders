24bf85e9b9148bee8d881f0265b270354178a9d9
Amarnath Maddireddy <amarnath@amreddys.com>
2022-10-24 16:04:03 +1100

    minor update to uplink-decoder

--- coreFunctions/uplink-decoder.cjs
+++ coreFunctions/uplink-decoder.cjs
@@ -8,39 +8,10 @@ function decodeUplink(input) {
     } else {
         let output = {
             sensorReading: readHex2bytes(input.bytes[3], input.bytes[4]),
-            temperatureReading: readHex2bytes(input.bytes[5], input.bytes[6]) / 100,
+            temperatureReading: readHex2bytes(input.bytes[5], input.bytes[6]),
             Battery: (input.bytes[7] * 0.1).toFixed(1)
         }
         return output
-        switch(deviceModel) {
-            case 'PLS2-L':
-                return {
-                    Level: output[sensorReading],
-                    Battery: output[Battery]
-                }
-            
-            case 'PTS2-L':
-            case 'PTC2-L':
-            case 'PTF2-L':
-            case 'PDS2-L':
-                return {
-                    Pressure: output[sensorReading],
-                    Battery: output[Battery]
-                }
-            
-            case 'PTD2-L':
-            case 'PDT2-L':
-                return {
-                    Pressure: output[sensorReading],
-                    Temperature: output[temperatureReading],
-                    Battery: output[Battery]
-                }
-            case 'DUS2-L':
-                return {
-                    Distance: output[sensorReading],
-                    Battery: output[Battery]
-                }
-        }
     }
 }
 
