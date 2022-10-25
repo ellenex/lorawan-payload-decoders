304b20ea8f63cf5f2932ce872954a4e6a2d7a717
Amarnath Maddireddy <amarnath@amreddys.com>
2022-10-26 10:44:03 +1100

    Updates to PLS2 sensor

--- Level Sensors/PLS2-L.js
+++ Level Sensors/PLS2-L.js
@@ -2,7 +2,7 @@ import { decodeUplink } from "../coreFunctions/uplink-decoder.cjs"
 import {changeSamplingTime} from "../coreFunctions/downlink-encoder.cjs"
 
 /*
-*   This section covers how to decode received data for Ellenex Level Sensor - Standard Model (PLS2-L)
+*   This section covers how to decode received data for Ellenex Standard Level Sensor (PLS2-L)
 */
 const decodePLS2 = function() {
     let sampleReceivedData = {
@@ -10,9 +10,26 @@ const decodePLS2 = function() {
         bytes: new Uint8Array([0,0,0,0,8,0,0,36])
     }
     let liquidDensity = 1 // Water = 1, Diesel = 0.85 
+    let sensorRange = 20 // 20 meters
+    const k = 0.01907
+    const m = 0.007
+    const b = -0.35
+
     console.log("Decoded data from hex: " + sampleReceivedData.bytes.map(x => x.toString(16).padStart(2, '0')).join(''))
     let decodedData = decodeUplink(sampleReceivedData)
-    console.log("Level: " + (decodedData.sensorReading / liquidDensity))
+
+    /*
+        Level calculation varies depending upon the type of your sensor. You will receive information about the type along with the EUI of the sensor.
+    */
+    let L1 = ((decodedData.temperatureReading - 1638.3) * sensorRange) / 13106.4
+    let L2 = (k * decodedData.sensorReading * m) + b
+    
+    let levelCalculationTypeOne = (L1 - (L2*10)) / liquidDensity
+    let levelCalculationTypeTwo = (sensorRange * (decodedData.sensorReading-4000)/16000) / liquidDensity
+    let levelCalculationTypeThree = (decodedData.sensorReading / liquidDensity)
+
+    console.log("Level: " + levelCalculationTypeThree)
+
     console.log("Battery: " + decodedData.Battery)
 }
 
