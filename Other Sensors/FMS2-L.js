ca01e455ba66fbc635ed7435f3a8a19239a2a259
Amarnath Maddireddy <amarnath@amreddys.com>
2023-03-10 14:27:13 +1100

    added FMS2

---
+++ Other Sensors/FMS2-L.js
@@ -0,0 +1,26 @@
+import { decodePulseCounterUplink } from "../coreFunctions/uplink-decoder.cjs"
+import {changeSamplingTime} from "../coreFunctions/downlink-encoder.cjs"
+
+/*
+*   This section covers how to decode received data for Ellenex Flow Meter Sensor (FMS2-L)
+*/
+const decodeFMS2 = function() {
+    let sampleReceivedData = {
+        fPort: 15,
+        bytes: new Uint8Array([0,22,38,0,0,91,34,36])
+    }
+    let hexStr = Array.from(sampleReceivedData.bytes, byte => ('0' + (byte & 0xFF).toString(16)).slice(-2)).join(' ')
+    console.log("Decoded data from hex: " + hexStr)
+    let decodedData = decodePulseCounterUplink(sampleReceivedData)
+    console.log("Total Pulses: " + decodedData.pulseCounts)
+    console.log("Flow Switch Spec: 2L per pulse")
+    console.log("Total Flow: " + ((decodedData.pulseCounts * 2)/10)+"L")
+    console.log("Battery: " + decodedData.Battery)
+}
+
+console.log("==============================================")
+decodeFMS2()
+console.log("==============================================")
+console.log("Change Sampling Time to 3 hours (180 minutes):")
+console.log(changeSamplingTime(180))
+console.log("==============================================")
